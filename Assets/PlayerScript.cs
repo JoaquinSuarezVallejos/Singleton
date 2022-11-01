2b2da640e39b56ef1453dd0642bd64ddca7594fa
PAX-12 <joaquin.suarezvallejos@gmail.com>
2022-11-01 12:15:39 -0300

    Initial commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public static PlayerScript instance;
+    public int puntajes;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
